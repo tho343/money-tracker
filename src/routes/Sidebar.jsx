18455b097038b587e739225c312c28f662eea7bb
Tran Ho <tranho8596@gmail.com>
2023-03-20 22:13:17 -0700

    toggle menu

--- src/routes/Sidebar.jsx
+++ src/routes/Sidebar.jsx
@@ -1,24 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { GiHamburgerMenu } from "react-icons/gi";
 import logo from '../assets/logo.svg';
 
 export default function Sidebar() {
+  const [showNavMenu, setShowNavMenu] = useState(false);
+  const toggleNav = (navBtn) => {
+    setShowNavMenu(!navBtn);
+  }
   return (
     <Fragment>
       <div className='sidebar-container'>
-        <img src={logo}></img>
+        <div className="sidebar-header">
+          <Link to="/" >
+            <img src={logo} alt="logo with two circles"></img>
+          </Link>
 
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/">
+          <GiHamburgerMenu className='toggle-btn' size={30} onClick={() => toggleNav(showNavMenu)} />
+        </div>
+
+        <div className={showNavMenu ? "nav-links-container show-links" : "nav-links-container"}>
+          <Link onClick={() => setShowNavMenu(false)} className="nav-link" to="/" >
             home
           </Link>
-          <Link className="nav-link" to="/income">
+          <Link onClick={() => setShowNavMenu(false)} className="nav-link" to="/income">
             income
           </Link>
-          <Link className="nav-link" to="/expenses">
+          <Link onClick={() => setShowNavMenu(false)} className="nav-link" to="/expenses">
             expenses
           </Link>
         </div>
+
 
       </div>
       <Outlet />
